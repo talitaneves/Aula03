29de59a6d9e27d22ec829088b0de8922d0e7a38e
Talita Neves <talita_manastella@hotmail.com>
2021-05-14 20:15:59 -0300

    Implementado o operador E

--- src/OperadorEComNumero.java
+++ src/OperadorEComNumero.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class OperadorE {
+public class OperadorEComNumero {
 
 	public static void main(String[] args) {
 		/*Faça um programa que verifique se o usuário digitado é igual a 'administrador' e se a senha digitada é igual a '123'*/
 		Scanner leitor = new Scanner(System.in);
-		String usuario, senha;
+		String usuario;
+		int senha;
 		System.out.println("SISTEMA DE LOGIN");
 		System.out.println("Por favor, digite seu username:");
 		usuario = leitor.next();
 		System.out.println("Por favor, digite sua senha:");
-		senha = leitor.next();	
+		senha = leitor.nextInt();	
 		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
 		
-		if (usuario.equalsIgnoreCase("administrador") && senha.equals("123")) {
+		if (usuario.equals("administrador") && senha == 123) {
 			System.out.println("Login realizado com sucesso");
 		}else {
 			System.out.println("Usuário ou senha incorretos");
